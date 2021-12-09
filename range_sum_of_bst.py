c06069bc54c31cb5bf856719205f330f19d34ec5
Huong Vo <87847334+huongtmvo@users.noreply.github.com>
2021-12-08 23:02:24 -0800

    solving range sum of BST
    https://leetcode.com/problems/range-sum-of-bst/submissions/
    Simply traverse the tree and discard left or right based on the range

---
+++ range_sum_of_bst.py
@@ -0,0 +1,16 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
+        if root is None:
+            return 0
+        ans = root.val if root.val >= low and root.val <= high else 0
+        if root.val >= low:
+            ans += self.rangeSumBST(root.left, low, high)
+        if root.val <= high:
+            ans += self.rangeSumBST(root.right, low, high)
+        return ans
